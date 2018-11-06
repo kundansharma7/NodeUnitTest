a1b37a1c28530d0ffb65f1e99c89a3d2ac3779a6
Kundan Kumar <kudankumar@SilverPushs-MacBook-Air.local>
2018-11-06 18:16:50 +0530

    add route

--- routes/route-user.js
+++ routes/route-user.js
@@ -5,4 +5,8 @@ module.exports = function(app) {
     app.get('/addUser', async (req, res) => {
         res.send("Add user route", req.body);
     });
+
+    app.get('/', async (req, res) => {
+        res.send("Sever is running");
+    });
 };
