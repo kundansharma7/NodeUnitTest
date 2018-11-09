cdb5ae3f90a565b8d5bc3e5bd8b73a09bf2d6888
Kundan Sharma <kundankumar.jayant@gmail.com>
2018-11-09 10:33:26 +0530

    Updated test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const expect = chai.expect;
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+      assert.equal([1,2,3].indexOf(2), -1);
     });
   });
 });
